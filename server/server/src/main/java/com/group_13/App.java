df86afe006e76c27ab72658e1196b3a9f0cec4fe
Joni Korpela <joni.korpela@student.oulu.fi>
2026-01-27 16:35:47 +0200

    Serverin kehitystä

--- server/server/src/main/java/com/group_13/App.java
+++ server/server/src/main/java/com/group_13/App.java
@@ -6,7 +6,7 @@ package com.group_13;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws Exception
     {
         System.out.println( "Hello World!" );
     }
