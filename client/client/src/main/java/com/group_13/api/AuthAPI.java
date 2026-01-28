e4f42b594f656ef43113d1ad75f899131fd7d9be
Joni Korpela <joni.korpela@student.oulu.fi>
2026-01-28 19:58:42 +0200

    client API draft

--- client/client/src/main/java/com/group_13/api/AuthAPI.java
+++ client/client/src/main/java/com/group_13/api/AuthAPI.java
@@ -1,7 +1,49 @@
 package com.group_13.api;
 
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.HashMap;
+import java.util.Map;
+
 import com.group_13.model.ApiResponse;
 
 public class AuthAPI extends BaseAPI {
-    // Authentication related API stuff goes here
+    private static final String AUTH_ENDPOINT = BASE_URL + "/auth/token";
+
+    public AuthAPI() {
+        super();
+    }
+
+    public ApiResponse login(String username, String password) throws Exception {
+        
+        Map<String, String> body = new HashMap<>();
+        body.put("username", username);
+        body.put("password", password);
+
+        String jsonBody = objectMapper.writeValueAsString(body);
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(java.net.URI.create(AUTH_ENDPOINT))
+                .header("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(jsonBody))
+                .build();
+
+        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+        return new ApiResponse(response.statusCode(), response.body(), response.headers().map());
+
+        // TODO: Set the token:
+        // setToken(extractedToken);
+    }
+
+    public ApiResponse logout(String token) throws Exception {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(java.net.URI.create(AUTH_ENDPOINT))
+                .header("Authorization", "Bearer " + token)
+                .DELETE()
+                .build();
+
+        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+        setToken(null);
+        return new ApiResponse(response.statusCode(), response.body(), response.headers().map());
+    }
 }
