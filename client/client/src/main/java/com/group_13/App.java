d9b5189975aed05654b7a6e9907c43c41b5d15f8
Henri Launonen <henri.launonen@hotmail.com>
2026-02-10 17:25:26 +0200

    poista turhaa

--- client/client/src/main/java/com/group_13/App.java
+++ client/client/src/main/java/com/group_13/App.java
@@ -1,20 +1,10 @@
 package com.group_13;
 
-import javax.swing.SwingUtilities;
 
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Client starting..." );
-
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                MainController.start();
-            }
-
-        });
 
 
     }
