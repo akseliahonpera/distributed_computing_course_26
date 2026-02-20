5abd307b4f1e39bfe7fc9b1859b922c279bd63b7
Ara <a.haulos@gmail.com>
2026-02-20 18:48:33 +0200

    forwarding post request fixed

--- client/client/src/main/java/com/group_13/api/BaseAPI.java
+++ client/client/src/main/java/com/group_13/api/BaseAPI.java
@@ -4,8 +4,6 @@ import java.net.http.HttpClient;
 import java.time.Duration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-
-import com.group_13.model.ApiResponse;
 import com.group_13.service.AuthService;
 
 /**
@@ -17,13 +15,7 @@ import com.group_13.service.AuthService;
 
 
 public abstract class BaseAPI {
-<<<<<<< HEAD
     protected static String BASE_URL = "http://127.0.0.1:8001/api"; // SET URL HERE
-=======
-
-    protected static final String BASE_URL = "http://127.0.0.1:8001/api"; // SET URL HERE
-
->>>>>>> 69d23e35feca1d55d334836d0e4bb3de0e6c226c
     protected static final HttpClient httpClient = HttpClient.newBuilder()
             .connectTimeout(Duration.ofSeconds(10))
             .build();
